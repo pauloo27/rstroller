0f780ef006465e980c30c052d3fd6a5859e3ab2b
Paulo <git@pauloo27.dev>
2024-02-18 15:55:02 -0300

    fix: add ellipsis, also add doc strings

--- common/src/utils.rs
+++ common/src/utils.rs
@@ -1,7 +1,14 @@
-pub fn truncate_string(s: &str, max_length: usize) -> &str {
-    if s.len() > max_length {
-        &s[..max_length]
+/// Truncate a string and add ellipsis if it's longer than `max_length`
+///
+/// # Output
+///
+/// The &str itself converted to a String if it's shorter than `max_length`
+/// or a truncated version with an ellipsis at the end.
+///
+pub fn truncate_string(s: &str, max_length: usize) -> String {
+    if s.len() <= max_length {
+        s.to_string()
     } else {
-        s
+        format!("{}...", &s[..max_length - 1])
     }
 }
