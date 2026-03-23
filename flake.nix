0f7eb872503704d0ebf606488f7a8a5150e74d90
Paulo <git@pauloo27.dev>
2026-03-23 16:26:09 -0300

    feat: flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "rstroller";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustc
+              cargo
+              rustfmt
+              clippy
+              rust-analyzer
+              pkg-config
+              dbus
+            ];
+          };
+        }
+      );
+    };
+}
