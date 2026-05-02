25623c63338f087b560547a94129bcdcc04449d2
Paulo <git@pauloo27.dev>
2026-05-02 20:27:59 -0300

    flake

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Rstroller - Player controller: cli, waybar, daemon and gui";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "rstroller";
+          version = "0.1.0";
+          doCheck = false; # some mpris tests are failing due to missing $DISPLAY
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            wrapGAppsHook4
+          ];
+
+          buildInputs = with pkgs; [
+            dbus
+            gtk4
+            openssl
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            openssl
+            rust-analyzer
+            pkg-config
+            gtk4
+            dbus
+          ];
+        };
+      }
+    );
+}
